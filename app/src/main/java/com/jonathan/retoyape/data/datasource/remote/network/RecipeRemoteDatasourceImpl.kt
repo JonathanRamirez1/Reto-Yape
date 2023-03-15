65c3e7e785cf450d1729e93e9819baa0b35560d6
jonathan-ramirez <j.ramirez@praxisglobe.net>
2023-03-14 22:57:01 -0500

    Se creo el Remote datasource y se inyecto

--- app/src/main/java/com/jonathan/retoyape/data/datasource/remote/network/RecipeRemoteDatasourceImpl.kt
+++ app/src/main/java/com/jonathan/retoyape/data/datasource/remote/network/RecipeRemoteDatasourceImpl.kt
@@ -1,10 +1,11 @@
 package com.jonathan.retoyape.data.datasource.remote.network
 
 import com.jonathan.retoyape.data.datasource.remote.model.RecipeModel
+import javax.inject.Inject
 
-class RecipeRemoteDatasourceImpl: RecipeRemoteDatasource {
+class RecipeRemoteDatasourceImpl @Inject constructor(private val recipeApi: RecipeApi): RecipeRemoteDatasource {
 
-    override suspend fun getRecipesFromApi(): List<RecipeModel> {
-
+    override suspend fun getRecipesFromApi(): List<RecipeModel>? {
+        return recipeApi.getRecipes().body()?.recipes
     }
 }
