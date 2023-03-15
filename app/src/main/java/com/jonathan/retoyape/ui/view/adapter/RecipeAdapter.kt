5ee04a2caf5ffc71466d7e4273818e3095756a8f
jonathan-ramirez <j.ramirez@praxisglobe.net>
2023-03-15 09:57:58 -0500

    Se agrego contenido al adapter

--- app/src/main/java/com/jonathan/retoyape/ui/view/adapter/RecipeAdapter.kt
+++ app/src/main/java/com/jonathan/retoyape/ui/view/adapter/RecipeAdapter.kt
@@ -1,4 +1,87 @@
 package com.jonathan.retoyape.ui.view.adapter
 
-class RecipeAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import coil.imageLoader
+import coil.request.ImageRequest
+import com.jonathan.retoyape.R
+import com.jonathan.retoyape.databinding.ItemRecipesBinding
+import com.jonathan.retoyape.domain.model.Recipe
+
+class RecipeAdapter(private val itemRecipes: ArrayList<Recipe>) :
+    RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
+
+    private lateinit var context: Context
+    private lateinit var navController: NavController
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeAdapter.RecipeViewHolder {
+        context = parent.context
+        return RecipeViewHolder(
+            ItemRecipesBinding.inflate(
+                LayoutInflater.from(context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecipeAdapter.RecipeViewHolder, position: Int) {
+        holder.bind(itemRecipes[position], context)
+        holder.itemView.setOnClickListener { view ->
+            val result = itemRecipes[position]
+
+            navController = Navigation.findNavController(view)
+            navController.navigate(
+                R.id.recipeDetailFragment,
+                bundleOf(
+                    "image" to result.image,
+                    "name" to result.name,
+                    "description" to result.description,
+                    "locationName" to result.locationName,
+                    "latitude" to result.latitude,
+                    "longitude" to result.longitude,
+                )
+            )
+        }
+    }
+
+    override fun getItemCount(): Int = itemRecipes.size
+
+    fun submitList(itemList: ArrayList<Recipe>) {
+        this.itemRecipes.clear()
+        this.itemRecipes.addAll(itemList)
+        notifyItemInserted(itemRecipes.size - 1)
+    }
+
+    inner class RecipeViewHolder(private val binding: ItemRecipesBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(recipes: Recipe, context: Context) {
+            val imageRequest = ImageRequest.Builder(context)
+                .data(recipes.image)
+                .crossfade(true)
+                .size(1280, 720)
+                .target(
+                    onStart = {
+                        binding.imageViewRecipes.setImageResource(R.drawable.ic_access_time)
+                    },
+                    onSuccess = { avatar ->
+                        binding.progressBar.visibility = View.GONE
+                        binding.imageViewRecipes.scaleType = ImageView.ScaleType.CENTER_CROP
+                        binding.imageViewRecipes.setImageDrawable(avatar)
+                    },
+                    onError = {
+                        binding.progressBar.visibility = View.GONE
+                        binding.imageViewRecipes.setImageResource(R.drawable.ic_load_error)
+                    }
+                )
+                .build()
+            context.imageLoader.enqueue(imageRequest)
+            binding.textViewName.text = recipes.name
+        }
+    }
 }
--- app/src/main/java/com/jonathan/retoyape/ui/view/fragment/RecipeFragment.kt
+++ app/src/main/java/com/jonathan/retoyape/ui/view/fragment/RecipeFragment.kt
@@ -5,15 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jonathan.retoyape.R
+import com.jonathan.retoyape.databinding.FragmentRecipeBinding
+import com.jonathan.retoyape.ui.view.adapter.RecipeAdapter
 
 class RecipeFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+    private lateinit var binding: FragmentRecipeBinding
+    private lateinit var recipeAdapter: RecipeAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
